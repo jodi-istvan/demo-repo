897d187ce0a21a8e70309e71d406a780f36ff10d
unknown <jodistvan21@gmail.com>
2021-11-28 13:01:58 +0200

    Request added to type_switcher.js

--- type_switcher.js
+++ type_switcher.js
@@ -1,5 +1,12 @@
 const field = document.getElementById("name")
 
 field.addEventListener('change', (e) => {
-    ///////////
+    const xhr = new XMLHttpRequest
+    xhr.open("GET", "blabla.api.php")
+
+    xhr.addEventListener('readystatechange', (e) => {
+        if (e.target.status === 200 && e.target.state === 4) {
+            // perform request
+        }
+    })
 })
